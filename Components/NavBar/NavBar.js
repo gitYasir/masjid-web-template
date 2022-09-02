bd35c534b67312d20f22e8a3792d6823d2160094
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-09-02 21:54:06 +0100

    working on navbar

--- Components/NavBar/NavBar.js
+++ Components/NavBar/NavBar.js
@@ -1,7 +1,12 @@
 import css from "./NavBar.module.scss";
 
 function NavBar() {
-  return <div className={css.mainContainer}>NavBar</div>;
+  return (
+    <div className={css.mainContainer}>
+      <h3>اسم المسجد</h3>
+      <h4>Masjid Name</h4>
+    </div>
+  );
 }
 
 export default NavBar;
