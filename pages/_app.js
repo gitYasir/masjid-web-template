a78691133f824bcd5bb7cb20833b2dab29c4bc02
Yasir <yasir.ibrahim@hotmail.co.uk>
2023-01-21 17:36:00 +0000

    added google anylytics and updated donate message

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-QZ75Y9EL3H"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-QZ75Y9EL3H');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
