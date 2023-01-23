27e48c0cbf92567e5d107e05cca2d8c86dd62197
Yasir <yasir.ibrahim@hotmail.co.uk>
2023-01-23 12:27:32 +0000

    update

--- Components/Madrasa/Madrasa.js
+++ Components/Madrasa/Madrasa.js
@@ -5,8 +5,8 @@ function Madrasa() {
     <div className={css.container}>
       <h2>Madrasa</h2>
       <p>
-        There is a madrasa available for 4 to 17 year olds in the evenings The
-        students are taught the Quran and tarbiya.
+        There is a madrasa available Monday to Friday, 5-6:30pm for 4 to 17 year
+        olds. The students are taught the Quran and tarbiya.
       </p>
     </div>
   );
