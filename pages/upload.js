355b6588b41d6f7fdbfe310e2b4fa6c30217564e
Yasir <yasir.ibrahim@hotmail.co.uk>
2023-05-09 06:20:03 +0100

    styled upload page and added preview image

--- pages/upload.js
+++ pages/upload.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 import supabase from "../Config/supabaseConfig";
 import { useRouter } from "next/router";
 import { v4 as uuidv4 } from "uuid";
-import Body from "../Components/Body/Body";
+import css from "../styles/upload.module.scss";
+import Image from "next/image";
 
 function Upload() {
   const [file, setFile] = useState([]);
   const [formError, setFormError] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
+
   const router = useRouter();
 
   const handleSubmit = async (e) => {
@@ -30,17 +33,49 @@ function Upload() {
     if (data) {
       setFormError(null);
     }
-    router.push("/");
+    // router.push("/");
   };
-
+  console.log(file);
   const handleFileSelected = (e) => {
+    const selectedFile = e.target.files[0];
     setFile(e.target.files[0]);
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const dataURL = reader.result;
+      setPreviewImage(dataURL);
+    };
+
+    reader.readAsDataURL(selectedFile);
   };
 
+  const X = () => {
+    return (
+      <>
+        {previewImage ? (
+          <Image src={previewImage} alt="preview" width={200} height={250} />
+        ) : (
+          <p>No file selected</p>
+        )}
+        {file && <p>{file.name}</p>}
+        <label htmlFor="file-upload" className={css.label}>
+          Choose Prayer Timetable
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          style={{ display: "none" }}
+          onChange={handleFileSelected}
+        />
+      </>
+    );
+  };
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" name="image" onChange={handleFileSelected} />
-      <button type="submit">Upload image</button>
+    <form onSubmit={handleSubmit} className={css.main}>
+      <X />
+      <button type="submit" className={css.btn}>
+        Upload Image
+      </button>
       {formError && <p>{formError}</p>}
     </form>
   );
