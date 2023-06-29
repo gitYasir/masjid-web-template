014e49379ed78c2d48edb2e54fe75ac8abe1c027
Yasir <yasir.ibrahim@hotmail.co.uk>
2023-06-29 01:38:44 +0100

    updated

--- Components/Notice/Notice.js
+++ Components/Notice/Notice.js
@@ -10,14 +10,6 @@ function Notice() {
           <h4>Darse Quran</h4>
           <p>Taking place every Friday after Maghrib prayer</p>
         </div>
-        <div className={css.eid}>
-          <Image
-            src={"/WhatsApp Image 2023-06-22 at 10.42.35 AM (1).jpeg"}
-            alt=""
-            layout="fill"
-            className={css.eidPic}
-          />
-        </div>
       </div>
     </div>
   );
